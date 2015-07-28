1b9daa6244d61c194eeb5a26e7e42a458952da60
Fays <faysalbouikila@free.fr>
2015-07-28 16:21:17 +0200

    Update localization_fr_rFR.lua

--- Mi-Fit/assets/lua/localization_fr_rFR.lua
+++ Mi-Fit/assets/lua/localization_fr_rFR.lua
@@ -1,6 +1,53 @@
 localization_fr_rFR_table = {
     calories_table={
-
+        {str="≈1个茶叶蛋",	calories=96},
+        {str="≈1片面包",	calories=100},
+        {str="≈1瓶冰红茶",	calories=112},
+        {str="≈1个煎蛋",	calories=118},
+        {str="≈2串羊肉串",	calories=124},
+        {str="≈1罐可乐",	calories=153},
+        {str="≈3串羊肉串",	calories=186},
+        {str="≈2个茶叶蛋",	calories=192},
+        {str="≈2片面包",	calories=200},
+        {str="≈1碗米饭",	calories=210},
+        {str="≈1个馒头",	calories=221},
+        {str="≈1根冰糖葫芦",	calories=223},
+        {str="≈2瓶冰红茶",	calories=224},
+        {str="≈1包小薯条",	calories=224},
+        {str="≈1个蛋黄派",	calories=224},
+        {str="≈2个煎蛋",	calories=236},
+        {str="≈4串羊肉串",	calories=248},
+        {str="≈1个汉堡",	calories=260},
+        {str="≈1根油条",	calories=270},
+        {str="≈1个肉包子",	calories=272},
+        {str="≈3个茶叶蛋",	calories=288},
+        {str="≈1个甜筒",	calories=290},
+        {str="≈3片面包",	calories=300},
+        {str="≈2罐可乐",	calories=306},
+        {str="≈1包中薯条",	calories=328},
+        {str="≈3个煎蛋",	calories=354},
+        {str="≈1个蛋挞",	calories=375},
+        {str="≈4片面包",	calories=400},
+        {str="≈1包大薯条",	calories=402},
+        {str="≈2碗米饭",	calories=420},
+        {str="≈2个馒头",	calories=442},
+        {str="≈2根冰糖葫芦",	calories=446},
+        {str="≈1个蛋黄派",	calories=448},
+        {str="≈1包方便面",	calories=470},
+        {str="≈5片面包",	calories=500},
+        {str="≈1盘蛋炒饭",	calories=516},
+        {str="≈2个汉堡",	calories=520},
+        {str="≈2根油条",	calories=540},
+        {str="≈2个肉包子",	calories=544},
+        {str="≈2个甜筒",	calories=580},
+        {str="≈1碗方便面",	calories=581},
+        {str="≈1碗牛肉拉面",	calories=593},
+        {str="≈10串羊肉串",	calories=620},
+        {str="≈3碗米饭",	calories=630},
+        {str="≈3个馒头",	calories=663},
+        {str="≈2个蛋挞",	calories=750},
+        {str="≈3根油条",	calories=810},
+        {str="≈3个肉包子",	calories=816},
     },
 
     ok='OK',
@@ -57,7 +104,6 @@ localization_fr_rFR_table = {
     manual_lazy_subtitle_used="Vous pouvez avoir plus de jours de repos en battant vos records personnels.",
     manual_fail_ytd_title = "Vous avez fini votre série aujourd'hui.",
     manual_fail_ytd_subtitle = "Votre série a été de %d.",
-
     active_time_format_0="%d h",
     active_time_format_1="%d h",
     active_time_format_2="%d h %d min",
@@ -65,7 +111,6 @@ localization_fr_rFR_table = {
     activie_run_format_1="Couru %s. Vous faites des miracles !",
     activie_run_format_2="Couru %s.",
     activie_run_consumed="%d calories brûlées. %s",
-
     get_distance_format=", distance totale de %d m.",
     get_distance_format_british="%d pieds",
     activity_walk_format="%s Marché %d pas %s",
@@ -78,33 +123,23 @@ localization_fr_rFR_table = {
     last_night_sleeped_good_format="La nuit dernière, vous avez dormi %s. Quelle bonne nuit !",
     last_night_sleeped_normal_format="La nuit dernière, vous avez dormi %s.",
     deep_sleep_format="En phase de sommeil profond pendant %s.",
-
     battery_low_info="Batterie faible, veuillez mettre en charge le Mi-Fit",
     battery_very_low_info="Batterie très faible, veuillez rapidement mettre en charge le Mi-Fit",
-
     cannot_find_bracelet="Impossible de trouver le Mi-Fit",
     cannot_find_bracelet_info="Assurez-vous que le Mi-Fit soit suffisamment chargé et à proximité de votre téléphone",
-
     bracelet_disconnect = "Problème de connexion ? Appuyez ici pour obtenir de l\'aide.",
-
     challenge_to_get="A %s jour(s) de battre un nouveau record !",
     record_reach_max="Vous avez atteint votre meilleur record !",
     new_record_born="Nouveau record !",
-
-    ---====== Weather tips ===============
     weather_tips_title_0 = "Forte pollution, essayez de rester à l'intérieur.",
     weather_tips_title_1 = "Forte pollution, essayez de rester à l'intérieur.",
     weather_tips_info = "Les abdominaux, la corde à sauter et les pompes sont de bons sports d'intérieur !",
-
-    ---------------Weight scale -------------------
     welcome_use_weight_scale = "Bonjour !",
     welcome_use_weight_scale_info = "En savoir plus sur la Mi-Balance",
     welcome_use_weight_scale_not_bind = "Appuyez ici pour appairer une Mi-balance",
     welcome_use_mi_health="Bonjour !",
-
     select_devices="Sélectionnez les appareils à appairer",
     daily_steps_not_comlete = "%s pas restants",
-
     am_format="%s AM",
     pm_format="%s PM",
 
